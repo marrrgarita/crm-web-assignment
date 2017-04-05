0a889e91613217a53893ab53f8da0161ac830492
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-04-05 15:26:07 -0400

    header and footer in layout, add links to index.erb

--- crm.rb
+++ crm.rb
@@ -1,12 +1,12 @@
 require_relative 'contact'
 require 'sinatra'
 
-# Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
-# Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
-# Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
+Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
+Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
+Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
 
 get '/' do
-  @crm_app_name = "Margarita's CRM"
+  @crm_app_name = "Margarita's Contacts"
   erb :index
 end
 
