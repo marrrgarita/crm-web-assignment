036a09403999d90800604efbd368a9a26ca7c15c
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-04-05 17:16:01 -0400

    create PUT method for edit form, replace old information with new information from edit form

--- crm.rb
+++ crm.rb
@@ -20,7 +20,7 @@ end
 
 post '/contacts' do
   Contact.create(params[:first_name], params[:last_name], params[:emails], params[:note])
-  redirect to('/contacts')
+  redirect to('/')
 end
 
 get '/contacts/:id' do
@@ -36,6 +36,20 @@ get '/contacts/:id/edit' do
   @contact = Contact.find(params[:id].to_i)
   if @contact
     erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    @contact.first_name = params[:first_name]
+    @contact.last_name = params[:last_name]
+    @contact.email = params[:email]
+    @contact.note = params[:note]
+
+    redirect to ('/')
   else
     raise Sinatra::NotFound
   end
