0cd903d0d64579dfe5eb40fbd19b2f30178f5b10
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-04-04 20:10:35 -0400

    implement all contacts method from Contact class to show the contacts on the web page

--- crm.rb
+++ crm.rb
@@ -1,4 +1,9 @@
+require_relative 'contact'
 require 'sinatra'
+
+Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
+Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
+Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
 
 get '/' do
   @crm_app_name = "Margarita's CRM"
@@ -6,6 +11,5 @@ get '/' do
 end
 
 get '/contacts' do
-  puts "test"
   erb :contacts
 end
